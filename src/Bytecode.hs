861b52171d469086044e2657ced13d2fb6c7fc41
Maxim Belov <maxim.belov@navinfo.eu>
2021-11-29 23:14:21 +0000

    added lenses

--- src/Bytecode.hs
+++ src/Bytecode.hs
@@ -14,7 +14,7 @@ import Text.Regex
 import Text.Read ( readMaybe )
 import System.IO 
 import System.Exit
-import Control.Lens ( set, view, over )
+import Control.Lens 
 import Control.Lens.TH (makeLenses)
 data Value = Undef | Integer Int | Float Float | Word String deriving (Show)
 
@@ -46,30 +46,22 @@ makeLenses ''IST
 {-------------  Runtime Actions ----------------------------}
 
 runtimeActions :: Dictionary ByteCodeOp
-runtimeActions = fromList [("+", rOp (+)), ("-" , rOp (-)), ("/", rOp (/)), ("*", rOp (*)), ("over", rOver), ("dup", rDup), ("swap", rSwap), (".", rDot), 
-            ("dump" , rDump), ("drop", rDrop), ("=", rComp (==)), (">", rComp (>)), ("<", rComp (<)), (",", rComa), ("@", rAt), ("!" , rBang), 
+runtimeActions = fromList [("+", rAdd), ("-" , rSub), ("/", rDiv), ("*", rMul), ("over", rOver), ("dup", rDup), ("swap", rSwap), (".", rDot), 
+            ("dump" , rDump), ("drop", rDrop), ("=", rEq), (">", rGt), ("<", rLt), (",", rComa), ("@", rAt), ("!" , rBang), 
             ("allot", rAllot), ("create", rCreate), ("does>", rDoes)]
 
-pop :: Stack Value -> Eval Value
-pop s = do
-    s <- gets s
-    case stackPop ds of
-        Just (s, x) -> return x
-        Nothing -> liftIO $ die "Attempted to pop on empty stack [" ++ s ++ "]"
+popd :: Eval Value
+popd = do
+    s <- get
+    case stackPop $ s^.ds of
+        Just (xs, x) -> return x
+        Nothing -> liftIO $ die "Attempted to pop on empty stack"
 
-push :: Stack Value -> Value -> Eval Value
-push s x = do 
-    ist <- get
-    set s x ist 
-    put ist
+pushd :: Value -> Eval Value
+pushd x = do 
+    s <- get
+    put s{ _ds = stackPush (s^.ds) x }
     return Undef
-
-peek :: Stack Value -> Eval Value
-peek s = do
-    s <- gets s
-    case stackPeek s of
-        Just x -> return x
-        Nothing -> return Undef
 
 rData :: String -> ByteCodeOp
 rData w = case readMaybe w :: Maybe Int of
@@ -78,105 +70,122 @@ rData w = case readMaybe w :: Maybe Int of
         Just f -> return (Float f)
         Nothing -> return (Word w)
 
-rOp :: ByteCodeOp
-rOp op = do 
-    ds <- gets _ds
-    b <- pop ds
-    a <- pop ds
+rAdd :: ByteCodeOp
+rAdd = do 
+    b <- popd
+    a <- popd
     case (a,b) of 
-        (Integer a', Integer b') -> push ds (Integer (op a' b'))
-        (Float a', Float b') -> push ds (Float (op a' b'))
+        (Integer a', Integer b') -> pushd (Integer (a' + b'))
+        (Float a', Float b') -> pushd (Float (a' + b'))
+    return Undef
+
+rSub :: ByteCodeOp
+rSub = do 
+    b <- popd
+    a <- popd
+    case (a,b) of 
+        (Integer a', Integer b') -> pushd (Integer (a' - b'))
+        (Float a', Float b') -> pushd (Float (a' - b'))
+    return Undef
+
+rMul :: ByteCodeOp
+rMul = do 
+    b <- popd
+    a <- popd
+    case (a,b) of 
+        (Integer a', Integer b') -> pushd (Integer (a' * b'))
+        (Float a', Float b') -> pushd (Float (a' * b'))
+    return Undef
+
+rDiv :: ByteCodeOp
+rDiv = do 
+    b <- popd
+    a <- popd
+    case (a,b) of 
+        (Integer a', Integer b') -> pushd (Integer (a' `div` b'))
+        (Float a', Float b') -> pushd (Float (a' / b'))
     return Undef
 
 rOver :: ByteCodeOp
 rOver = do
-    ds <- gets _ds
-    a <- pop ds
-    b <- pop ds
-    push ds b
-    push ds a
-    push ds b
+    a <- popd
+    b <- popd
+    pushd b
+    pushd a
+    pushd b
     return Undef 
 
 rDup :: ByteCodeOp
 rDup = do
-    ds <- gets _ds
-    a <- pop ds
-    push ds a
-    push ds a
+    a <- popd
+    pushd a
+    pushd a
     return Undef
 
 rSwap :: ByteCodeOp
 rSwap = do
-    ds <- gets _ds
-    a <- pop ds
-    b <- pop ds
-    push ds a
-    push ds b
+    a <- popd
+    b <- popd
+    pushd a
+    pushd b
     return Undef
 
 rDot :: ByteCodeOp
 rDot = do
-    ds <- gets _ds
-    a <- pop ds
+    a <- popd
     liftIO $ print a
     return Undef
 
 rDump :: ByteCodeOp
 rDump = do
-    ds <- gets _ds
-    liftIO $ print ds
+    s <- get
+    liftIO $ print $ s ^. ds
     return Undef
 
 rDrop :: ByteCodeOp
-rDrop = gets _ds >>= pop >> return Undef
+rDrop = popd >> return Undef
 
 rJmp :: ByteCodeOp
-rJmp = do
-    ist <- get
-    addr <- pcode ist !! pp ist
-    case addr of
-        Integer a' ->  put ist { pp = a' } >> return Undef
-        _ -> liftIO $ die "Incorrect JUMP address."
+rJmp = get >>= put >> return Undef
 
 rJnz :: ByteCodeOp
-rJnz = do
-    ist <- get
-    addr <- pcode ist !! pp ist
-    x <- pop ds ist
-    case (x, addr) of 
-        (Integer 0, _) -> put ist { _pp = _pp ist + 1 } >> return Undef
-        (Integer _, Integer a') -> put ist { _pp = a' } >> return Undef
+rJnz = get >>= put >> return Undef
 
 rJz :: ByteCodeOp
-rJz = do
-    ist <- get
-    addr <- pcode ist !! pp ist
-    x <- pop _ds ist
-    case (x, addr) of 
-        (Integer 0, Integer a') -> put ist { _pp = a' } >> return Undef
-        _ -> put ist { _pp = _pp ist + 1 } >> return Undef
+rJz = get >>= put >> return Undef
 
-rComp :: ByteCodeOp
-rComp op = do
-    ds <- gets _ds
-    b <- pop _ds
-    a <- pop _ds
+rEq :: ByteCodeOp
+rEq = do
+    b <- popd
+    a <- popd
     case (a,b) of 
-        (Integer a', Integer b') -> push ds (Integer (if op a' b' then 1 else 0))
-        (Float a', Float b') -> push ds (Float (if op a' b' then 1 else 0))
+        (Integer a', Integer b') -> pushd (Integer (if a' == b' then 1 else 0))
+        (Float a', Float b') -> pushd (Float (if a' == b' then 1 else 0))
+    return Undef
+
+rGt :: ByteCodeOp
+rGt = do
+    b <- popd
+    a <- popd
+    case (a,b) of 
+        (Integer a', Integer b') -> pushd (Integer (if a' > b' then 1 else 0))
+        (Float a', Float b') -> pushd (Float (if a' > b' then 1 else 0))
+    return Undef
+
+rLt :: ByteCodeOp
+rLt = do
+    b <- popd
+    a <- popd
+    case (a,b) of 
+        (Integer a', Integer b') -> pushd (Integer (if a' < b' then 1 else 0))
+        (Float a', Float b') -> pushd (Float (if a' < b' then 1 else 0))
     return Undef
 
 rComa :: ByteCodeOp
 rComa = get >>= put >> return Undef 
 
 rAt :: ByteCodeOp
-rAt = do
-    ist <- get
-    addr <- pop _ds ist
-    case addr of
-        Integer a -> push (_ds ist) (heap ist !! a)
-        _ -> liftIO $ die "Heap address is incorrect."
+rAt = get >>= put >> return Undef
 
 rBang :: ByteCodeOp
 rBang = get >>= put >> return Undef 
@@ -194,18 +203,10 @@ rRun :: ByteCodeOp
 rRun = get >>= put >> return Undef
 
 rPush :: ByteCodeOp
-rPush = do
-    ist <- get
-    d <- _pcode ist !! _pp ist
-    push (ds ist) d
-    put ist { _pp = _pp ist + 1 }
-    return Undef
+rPush = get >>= put >> return Undef
 
 exec :: Eval Value
-exec = do
-        ist <- get
-        if pp ist == length (pcode ist) then compile
-        else put ist { _pp = _pp ist + 1 } >> _pcode ist !! _pp ist >> exec
+exec = get >>= put >> return Undef
 
 {-------------  Compile-time Actions ----------------------------}
 compilerActions :: Dictionary ByteCodeOp
@@ -213,16 +214,6 @@ compilerActions = fromList [(":", cColon), (";", cSemi), ("if", cIf), ("else", c
 
 cColon :: ByteCodeOp
 cColon = get >>= put >> return Undef
-{- cColon = do
-    ist <- get
-    if stackIsEmpty (cs ist) then do
-        case program ist of
-            [] -> do
-                c <- liftIO $ moreCode "... " 
-                put ist { _cs = stackPush (_cs ist) (head c), _program = tail c }
-                return Undef
-            (x:xs) -> put ist { _cs = stackPush (_cs ist) x, _program = xs } >> return Undef     
-    else liftIO $ die "Control Stack is not empty while trying to do : ." -}
 
 cSemi :: ByteCodeOp
 cSemi = get >>= put >> return Undef
@@ -247,17 +238,7 @@ runEval eval = runStateT (runContT eval return)
 
 evalProg :: String -> IO (Value, IST)
 evalProg program = 
-    let st = IST {
-        _pp = 0,
-        _hp = 0,
-        _ds = stackNew,
-        _cs = stackNew,
-        _heap = [],
-        _program = lexer program,
-        _pcode = [],
-        _dd = empty
-    } 
-    in
+    let st = IST 0 0 stackNew stackNew [] (lexer program) [] empty in
         runEval compile st
 
 moreCode :: String -> IO [ String ]
@@ -270,7 +251,7 @@ compile :: Eval Value
 compile = get >>= \case
         IST { _program = [], _pcode = [] } -> do
             c <- liftIO $ moreCode "bc> " 
-            modify $ \ist -> ist { _program = c }
+            modify $ \s -> s { _program = c }
             compile
         IST { _program = (x:xs), _pcode = [] } -> if x == ":bye" 
                                                 then do
@@ -280,27 +261,27 @@ compile = get >>= \case
                                                          Just op -> op >> compile
                                                          Nothing -> case M.lookup x runtimeActions of 
                                                                         Just op -> do
-                                                                            modify $ \ist -> ist { _pcode = _pcode ist ++ [op] }
+                                                                            modify $ \s -> s { _pcode = s^.pcode ++ [op] }
                                                                             compile
                                                                         Nothing -> do
                                                                             dd <- gets _dd
                                                                             case M.lookup x dd of
                                                                                 Just bc -> do
-                                                                                    modify $ \ist -> ist { _pcode = _pcode ist ++ [rRun, rData x] }
+                                                                                    modify $ \s -> s { _pcode = s^.pcode ++ [rRun, rData x] }
                                                                                     compile
                                                                                 Nothing -> do
                                                                                     y <- rData x
                                                                                     case y of
-                                                                                        Integer i -> modify $ \ist -> ist { _pcode = _pcode ist ++ [rPush, rData x] }
-                                                                                        Float d -> modify $ \ist -> ist { _pcode = _pcode ist ++ [rPush, rData x] }
-                                                                                        Word w -> modify $ \ist -> ist { _pcode = _pcode ist ++ [rRun, rData x] }
+                                                                                        Integer i -> modify $ \s -> s { _pcode = s ^. pcode ++ [rPush, rData x] }
+                                                                                        Float d -> modify $ \s -> s { _pcode = s ^. pcode ++ [rPush, rData x] }
+                                                                                        Word w -> modify $ \s -> s { _pcode = s ^. pcode ++ [rRun, rData x] }
                                                                                     compile
-        IST { _pcode = (x:xs) } -> do 
-                                      cs <- gets _cs
-                                      if stackIsEmpty cs then exec
+        IST { _pcode = (x:xs) } -> do  
+                                      s <- get
+                                      if stackIsEmpty (s ^. cs) then exec
                                       else do
                                           c <- liftIO $ moreCode "... " 
-                                          modify $ \ist -> ist { _program = c }
+                                          modify $ \s -> s { _program = c }
                                           compile
 
 lexer :: String -> [ String ]
